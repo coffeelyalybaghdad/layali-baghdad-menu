65e82dc2a1112e6290749662da32fc374234ebe1
coffeelyalybaghdad <coffeelyalybaghdad@gmail.com>
2026-07-19 19:42:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,102 @@
 const products = [
+
+/* =========================
+   فرابيه & آيس
+========================= */
+
+{
+name:"آيس ليالي",
+price:"5000 د.ع",
+category:"ice",
+image:"images/ice1.jpg"
+},
+{
+name:"آيس كوفي",
+price:"3000 د.ع",
+category:"ice",
+image:"images/ice2.jpg"
+},
+{
+name:"فرابيه شوكولاتة",
+price:"3500 د.ع",
+category:"ice",
+image:"images/frappe1.jpg"
+},
+{
+name:"فرابيه فانيلا",
+price:"3500 د.ع",
+category:"ice",
+image:"images/frappe2.jpg"
+},
+
+/* =========================
+   ميلك شيك
+========================= */
+
+{
+name:"ميلك شيك ليالي",
+price:"5000 د.ع",
+category:"milkshake",
+image:"images/milkshake1.jpg"
+},
+{
+name:"ميلك شيك من السما",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake2.jpg"
+},
+{
+name:"ميلك شيك كندر",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake3.jpg"
+},
+{
+name:"ميلك شيك لوتس",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake4.jpg"
+},
+{
+name:"ميلك شيك أوريو",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake5.jpg"
+},
+{
+name:"ميلك شيك فراولة",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake6.jpg"
+},
+{
+name:"ميلك شيك سنيكرز",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake7.jpg"
+},
+{
+name:"ميلك شيك نوتيلا",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake8.jpg"
+},
+{
+name:"ميلك شيك كيت كات",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake9.jpg"
+},
+{
+name:"ميلك شيك فانيلا",
+price:"4000 د.ع",
+category:"milkshake",
+image:"images/milkshake10.jpg"
+},
+/* =========================
+   كريب
+========================= */
+
 {
 name:"كريب ليالي",
 price:"6000 د.ع",
@@ -46,81 +144,389 @@ name:"كريب فواكه",
 price:"5000 د.ع",
 category:"crepe",
 image:"images/crepe8.jpg"
-}
+},
+
+/* =========================
+   وافل
+========================= */
+
 {
-name:"وافل نوتيلا",
-price:"7000 د.ع",
+name:"وافل ليالي",
+price:"6000 د.ع",
 category:"waffle",
 image:"images/waffle1.jpg"
 },
 {
-name:"بان كيك",
-price:"8000 د.ع",
-category:"pancake",
-image:"images/pancake1.jpg"
-},
-{
-name:"ميلك شيك أوريو",
-price:"7000 د.ع",
-category:"milkshake",
-image:"images/milkshake1.jpg"
-},
-{
-name:"فرابيه كراميل",
-price:"7000 د.ع",
-category:"ice",
-image:"images/frappe1.jpg"
-},
-{
-name:"آيس لاتيه",
-price:"6000 د.ع",
-category:"ice",
-image:"images/ice1.jpg"
-},
-{
-name:"موهيتو بلو",
+name:"وافل نوتيلا",
 price:"5000 د.ع",
-category:"mojito",
-image:"images/mojito1.jpg"
+category:"waffle",
+image:"images/waffle2.jpg"
 },
 {
-name:"عصير برتقال",
+name:"وافل بستاشيو",
 price:"5000 د.ع",
-category:"juice",
-image:"images/juice1.jpg"
+category:"waffle",
+image:"images/waffle3.jpg"
 },
 {
-name:"قهوة عربية",
+name:"وافل لوتس",
+price:"5000 د.ع",
+category:"waffle",
+image:"images/waffle4.jpg"
+},
+/* =========================
+   قهوة
+========================= */
+
+{
+name:"قهوة تركية",
 price:"2000 د.ع",
 category:"coffee",
 image:"images/coffee1.jpg"
 },
 {
-name:"قهوة تركية",
-price:"3000 د.ع",
+name:"قهوة عثمانية",
+price:"2000 د.ع",
 category:"coffee",
 image:"images/coffee2.jpg"
 },
 {
-name:"هوت شوكليت",
-price:"5000 د.ع",
+name:"قهوة نوتيلا",
+price:"2000 د.ع",
+category:"coffee",
+image:"images/coffee3.jpg"
+},
+{
+name:"قهوة بالحليب",
+price:"2000 د.ع",
+category:"coffee",
+image:"images/coffee4.jpg"
+},
+{
+name:"قهوة بستاشيو",
+price:"2000 د.ع",
+category:"coffee",
+image:"images/coffee5.jpg"
+},
+{
+name:"قهوة عربية",
+price:"2000 د.ع",
+category:"coffee",
+image:"images/coffee6.jpg"
+},
+{
+name:"قهوة جكليتية",
+price:"2000 د.ع",
+category:"coffee",
+image:"images/coffee7.jpg"
+},
+
+/* =========================
+   مشروبات ساخنة
+========================= */
+
+{
+name:"شاي",
+price:"500 د.ع",
 category:"hot",
 image:"images/hot1.jpg"
 },
 {
-name:"مكسيكي",
-price:"6000 د.ع",
+name:"شاي بالجوز",
+price:"1000 د.ع",
+category:"hot",
+image:"images/hot2.jpg"
+},
+{
+name:"شاي بالحليب",
+price:"1000 د.ع",
+category:"hot",
+image:"images/hot3.jpg"
+},
+{
+name:"كابتشينو",
+price:"2000 د.ع",
+category:"hot",
+image:"images/hot4.jpg"
+},
+{
+name:"حليب",
+price:"2000 د.ع",
+  category:"hot",
+image:"images/hot5.jpg"
+},
+{
+name:"نسكافيه",
+price:"2000 د.ع",
+category:"hot",
+image:"images/hot6.jpg"
+},
+{
+name:"هوت جوكليت",
+price:"2000 د.ع",
+category:"hot",
+image:"images/hot7.jpg"
+},
+{
+name:"حليب بالمكسرات",
+price:"2000 د.ع",
+category:"hot",
+image:"images/hot8.jpg"
+},
+{
+name:"حليب بالعسل",
+price:"2000 د.ع",
+category:"hot",
+image:"images/hot9.jpg"
+},
+{
+name:"سحلب",
+price:"2000 د.ع",
+category:"hot",
+image:"images/hot10.jpg"
+},
+{
+name:"كابتشينو بالكراميل",
+price:"2000 د.ع",
+category:"hot",
+image:"images/hot11.jpg"
+},
+/* =========================
+   موهيتو
+========================= */
+
+{
+name:"موهيتو ليالي",
+price:"5000 د.ع",
+category:"mojito",
+image:"images/mojito1.jpg"
+},
+{
+name:"موهيتو فراولة",
+price:"3000 د.ع",
+category:"mojito",
+image:"images/mojito2.jpg"
+},
+{
+name:"موهيتو كيوي",
+price:"3000 د.ع",
+category:"mojito",
+image:"images/mojito3.jpg"
+},
+{
+name:"موهيتو بلو",
+price:"3000 د.ع",
+category:"mojito",
+image:"images/mojito4.jpg"
+},
+{
+name:"موهيتو أناناس",
+price:"3000 د.ع",
+category:"mojito",
+image:"images/mojito5.jpg"
+},
+{
+name:"موهيتو ليمون ونعناع",
+price:"3000 د.ع",
+category:"mojito",
+image:"images/mojito6.jpg"
+},
+{
+name:"موهيتو رمان",
+price:"3000 د.ع",
+category:"mojito",
+image:"images/mojito7.jpg"
+},
+
+/* =========================
+   عصائر
+========================= */
+
+{
+name:"عصير ليالي",
+price:"5000 د.ع",
+category:"juice",
+image:"images/juice1.jpg"
+},
+{
+name:"عصير كوكتيل",
+price:"3500 د.ع",
+category:"juice",
+image:"images/juice2.jpg"
+},
+{
+name:"عصير برتقال",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice3.jpg"
+},
+{
+name:"عصير ليمون",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice4.jpg"
+},
+{
+name:"عصير ليمون ونعناع",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice5.jpg"
+},
+{
+name:"عصير أناناس",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice6.jpg"
+},
+{
+name:"عصير بطيخ",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice7.jpg"
+},
+{
+name:"عصير فراولة",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice8.jpg"
+},
+{
+name:"عصير مانجا",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice9.jpg"
+},
+{
+name:"عصير كيوي",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice10.jpg"
+},
+{
+name:"عصير موز وفراولة",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice11.jpg"
+},
+{
+name:"عصير موز ونوتيلا",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice12.jpg"
+},
+{
+name:"عصير موز وحليب",
+price:"3000 د.ع",
+category:"juice",
+image:"images/juice13.jpg"
+},
+{
+name:"عصير حمضيات",
+price:"3500 د.ع",
+category:"juice",
+image:"images/juice14.jpg"
+},
+/* =========================
+   مكسيكي
+========================= */
+
+{
+name:"مكسيكي ليالي",
+price:"5000 د.ع",
 category:"mexican",
 image:"images/mexican1.jpg"
 },
 {
-name:"أركيلة",
-price:"8000 د.ع",
+name:"مكسيكي رمان",
+price:"3000 د.ع",
+category:"mexican",
+image:"images/mexican2.jpg"
+},
+{
+name:"مكسيكي ليمون",
+price:"3000 د.ع",
+category:"mexican",
+image:"images/mexican3.jpg"
+},
+{
+name:"مكسيكي برتقال",
+price:"3000 د.ع",
+category:"mexican",
+image:"images/mexican4.jpg"
+},
+{
+name:"مكسيكي بلو",
+price:"3000 د.ع",
+category:"mexican",
+image:"images/mexican5.jpg"
+},
+{
+name:"مكسيكي تفاح",
+price:"3000 د.ع",
+category:"mexican",
+image:"images/mexican6.jpg"
+},
+{
+name:"مكسيكي فراولة",
+price:"3000 د.ع",
+category:"mexican",
+image:"images/mexican7.jpg"
+},
+
+/* =========================
+   أراكيل
+========================= */
+
+{
+name:"أركيلة تفاحتين خلطة",
+price:"7000 د.ع",
 category:"hookah",
 image:"images/hookah1.jpg"
+},
+{
+name:"أركيلة تفاحتين نخلة",
+price:"7000 د.ع",
+category:"hookah",
+image:"images/hookah2.jpg"
+},
+{
+name:"أركيلة علك ونعناع",
+price:"7000 د.ع",
+category:"hookah",
+image:"images/hookah3.jpg"
+},
+{
+name:"أركيلة ليالي",
+price:"7000 د.ع",
+category:"hookah",
+image:"images/hookah4.jpg"
+},
+{
+name:"أركيلة ليمون",
+price:"7000 د.ع",
+category:"hookah",
+image:"images/hookah5.jpg"
+},
+{
+name:"أركيلة إنكليزي",
+price:"7000 د.ع",
+category:"hookah",
+image:"images/hookah6.jpg"
+},
+{
+name:"أركيلة باونتي",
+price:"7000 د.ع",
+category:"hookah",
+image:"images/hookah7.jpg"
+},
+{
+name:"أركيلة علك وبطيخ",
+price:"7000 د.ع",
+category:"hookah",
+image:"images/hookah8.jpg"
 }
-];
 
+];
 const menu=document.getElementById("menu");
 const search=document.getElementById("search");
 const buttons=document.querySelectorAll(".categories button");
@@ -131,14 +537,14 @@ const close=document.getElementById("close");
 function showProducts(list){
 menu.innerHTML="";
 list.forEach(item=>{
-menu.innerHTML+=`
+menu.innerHTML+=
 <div class="card">
-<img src="${item.image}" onclick="openImage('${item.image}')">
+<img src="${item.image}" alt="${item.name}" onclick="openImage('${item.image}')">
 <div class="info">
 <h3>${item.name}</h3>
 <span class="price">${item.price}</span>
 </div>
-</div>`;
+</div>;
 });
 }
 
@@ -159,10 +565,12 @@ showProducts(products.filter(p=>p.category===cat));
 };
 });
 
-search.onkeyup=()=>{
-const value=search.value.toLowerCase();
-showProducts(products.filter(p=>p.name.toLowerCase().includes(value)));
-};
+search.addEventListener("keyup",()=>{
+const value=search.value.trim().toLowerCase();
+showProducts(
+products.filter(p=>p.name.toLowerCase().includes(value))
+);
+});
 
 function openImage(src){
 viewer.classList.add("show");
